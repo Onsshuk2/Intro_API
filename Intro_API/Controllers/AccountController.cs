0f9fe0390fee885ed1d699e8d59f5bc2628ba11a
Іра Оніщук <onsshhuk086@gmail.com>
2025-03-07 11:48:04 +0000

    dz 2

---
+++ Intro_API/Controllers/AccountController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Intro_API.BLL.DTOs.Account;
+using Intro_API.BLL.Services.Account;
+
+namespace Intro_API.Controllers
+{
+    [ApiController]
+    [Route("api/account")]
+    public class AccountController : Controller
+    {
+        private IAccountService _accountService;
+
+        public AccountController(IAccountService accountService)
+        {
+            _accountService = accountService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> LoginAsync([FromBody] LoginDto dto)
+        {
+            var result = await _accountService.LoginAsync(dto);
+
+            if (result == null)
+                return BadRequest("Incorrect userName or password");
+
+            return Ok(result);
+        }
+    }
--- Intro_API/Program.cs
+++ Intro_API/Program.cs
@@ -1,10 +1,14 @@
-using Intro_API.DAL;
 using Microsoft.EntityFrameworkCore;
-using System;
+using Intro_API.BLL.Services.Account;
+using Intro_API.BLL.Services.Role;
+using Intro_API.DAL;
+using static Intro_API.DAL.Entities.Role;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddScoped<IAccountService, AccountService>();
+builder.Services.AddScoped<IRoleService, RoleService>();
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
@@ -14,6 +18,19 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 {
     options.UseNpgsql("name=NpgsqlLocal");
 });
+
+// Add identity
+builder.Services
+    .AddIdentity<AppUser, AppRole>(options =>
+    {
+        options.Password.RequireNonAlphanumeric = false;
+        options.Password.RequiredUniqueChars = 0;
+        options.Password.RequireDigit = false;
+        options.Password.RequiredLength = 6;
+        options.Password.RequireLowercase = false;
+        options.Password.RequireUppercase = false;
+    })
+    .AddEntityFrameworkStores<AppDbContext>();
 
 //builder.Services.AddOpenApi();
 builder.Services.AddSwaggerGen();
